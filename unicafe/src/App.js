aa1d737d4ecec5483f97d5771812cde01470f14c
Petar Petrov <ppetrovbusiness@gmail.com>
2023-05-07 21:29:35 +0300

    unicafe - 1.6 task

---
+++ unicafe/src/App.js
@@ -0,0 +1,100 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const voteGood = () => (setGood(good + 1))
+  const voteNeutral = () => setNeutral(neutral + 1);
+  const voteBad = () => setBad(bad + 1);
+
+  const buttons = [
+    {
+      value: "Good",
+      handleClick: voteGood,
+    },
+    {
+      value: "Neutral",
+      handleClick: voteNeutral,
+    },
+    {
+      value: "Bad",
+      handleClick: voteBad,
+    }
+  ];
+
+  const votes = [
+    {
+      type: "Good",
+      amount: good,
+    },
+    {
+      type: "Neutral",
+      amount: neutral,
+    },
+    {
+      type: "Bad",
+      amount: bad,
+    },
+  ]
+
+  return (
+    <div>
+      <Feedback buttons={buttons} />
+      <Statistics votes={votes}/>
+    </div>
+  )
+}
+
+
+const Feedback = ({ buttons }) => {
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      {buttons.map(button => {
+        return (
+          <FeedbackBtn key={button.value} value={button.value} handleClick={button.handleClick} />
+        )
+      })}
+    </div>
+  )
+}
+
+const FeedbackBtn = ({ value, handleClick }) => {
+  return (
+    <button onClick={handleClick}>
+      {value}
+    </button>
+  )
+}
+
+const Statistics = ({votes}) => {
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      {
+        votes.map(vote => {
+          return (
+            <VoteStats key={vote.type} type={vote.type} amount={vote.amount}/>
+          )
+        })
+      }
+    </div>
+  )
+
+}
+
+const VoteStats = ({type, amount}) => (
+  <div>
+    {type} {amount}
+  </div>
+)
+
+
+
+export default App
