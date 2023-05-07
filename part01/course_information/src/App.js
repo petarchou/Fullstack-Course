9196ecb065b2d3c5a2716d6c86deee3b77867837
Petar Petrov <ppetrovbusiness@gmail.com>
2023-05-07 15:39:23 +0300

    Finish 1.5

--- part01/course_information/src/App.js
+++ part01/course_information/src/App.js
@@ -1,6 +1,8 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts =[{
+  const course = {
+  name: 'Half Stack application development',
+  parts:[
+  {
     name: 'Fundamentals of React',
    exercises: 10,
   },
@@ -11,14 +13,15 @@ const App = () => {
   {
     name: 'State of a component',
    exercises: 14,
-  }];
+  }]
+};
 
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
