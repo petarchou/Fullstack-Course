d942ef84ed4a5a577a8f82140a8fe0ab0e4f3b41
Petar Petrov <ppetrovbusiness@gmail.com>
2023-05-08 12:53:00 +0300

    2.1 course-information

---
+++ part02/course-information/src/App.js
@@ -0,0 +1,67 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+const Course = ({course}) => {
+
+  return (
+    <div>
+      <Header title={course.name}/>
+      <Content parts={course.parts}/>
+    </div>
+  )
+}
+
+
+const Header = ({title}) => {
+
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map(part => <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+
+const Part = ({part}) => {
+  return (
+    <div>
+      <h3>
+        {part.name}
+      </h3>
+      <p>Exercises: {part.exercises}</p>
+      <hr></hr>
+    </div>
+  )
+}
+
+export default App
