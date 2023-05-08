e5a8ba816faaddb5ca7d278866967ebecda3e3b3
Petar Petrov <ppetrovbusiness@gmail.com>
2023-05-08 12:55:50 +0300

    2.2 add total exercises

--- part02/course-information/src/App.js
+++ part02/course-information/src/App.js
@@ -24,18 +24,18 @@ const App = () => {
   return <Course course={course} />
 }
 
-const Course = ({course}) => {
+const Course = ({ course }) => {
 
   return (
     <div>
-      <Header title={course.name}/>
-      <Content parts={course.parts}/>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
     </div>
   )
 }
 
 
-const Header = ({title}) => {
+const Header = ({ title }) => {
 
   return (
     <header>
@@ -44,15 +44,27 @@ const Header = ({title}) => {
   )
 }
 
-const Content = ({parts}) => {
+const Content = ({ parts }) => {
   return (
     <div>
       {parts.map(part => <Part key={part.id} part={part} />)}
+      <Aggregations parts={parts} />
     </div>
   )
 }
 
-const Part = ({part}) => {
+const Aggregations = ({ parts }) => {
+  const totalExercises = parts.map(part => part.exercises)
+    .reduce((prev, curr) => prev + curr, 0);
+
+  return (
+    <div>
+      <h4>Total exercises: {totalExercises}</h4>
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
   return (
     <div>
       <h3>
