ac63922e2e64b831a965aae189b945784d49dd29
Petar Petrov <ppetrovbusiness@gmail.com>
2023-05-08 19:39:11 +0300

    styling

--- part02/lab-exercises/notes-app/src/App.js
+++ part02/lab-exercises/notes-app/src/App.js
@@ -1,6 +1,7 @@
 import Note from './components/Note'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Notification from './components/Notification'
 
 import notesService from './services/notes'
 
@@ -9,7 +10,8 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
-  
+  const [errorMessage, setErrorMessage] = useState(null);
+
     const hook = () => {
       notesService.getAll()
         .then(initialNotes => {
@@ -56,9 +58,12 @@ const App = () => {
       setNotes(notes.map(note => note.id !== id ? note : responseNote));
     })
     .catch(err => {
-      alert(
-        `the note '${note.content}' was already deleted from server.`
-      );
+      setErrorMessage(
+        `Note '${note.content}' has been removed from server`
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
       setNotes(notes.filter(n => n.id !== note.id));
     })
   }
@@ -66,6 +71,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -85,6 +91,21 @@ const App = () => {
           placeholder='add a new note...' />
         <button type="submit">save</button>
       </form>
+      <Footer />
+    </div>
+  )
+}
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2022</em>
     </div>
   )
 }
--- part02/lab-exercises/notes-app/src/index.js
+++ part02/lab-exercises/notes-app/src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom/client"
 import App from "./App"
-
 import axios from 'axios'
+
+import './index.css'
 
 
     ReactDOM.createRoot(document.getElementById("root")).render(
