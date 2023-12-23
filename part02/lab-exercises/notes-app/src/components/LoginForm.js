a216242426fab734de73f31d970d8917b9693a9e
Magellan <ppetrov12@abv.bg>
2023-12-24 00:10:13 +0200

    create loginForm component

---
+++ part02/lab-exercises/notes-app/src/components/LoginForm.js
@@ -0,0 +1,26 @@
+const loginForm = ({handleLogin, username, setUsername, password, setPassword}) => {
+    return (
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type='text'
+              value={username}
+              name='Username'
+              onChange={({ target }) => setUsername(target.value)}></input>
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+    )
+}
+
+export default loginForm
