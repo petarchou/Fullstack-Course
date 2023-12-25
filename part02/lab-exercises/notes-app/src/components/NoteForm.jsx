8b6f16928af1fd91b60ed78b7e673daeb36b19f4
Magellan <ppetrov12@abv.bg>
2023-12-25 09:34:25 +0200

    eslint

--- part02/lab-exercises/notes-app/src/components/NoteForm.jsx
+++ part02/lab-exercises/notes-app/src/components/NoteForm.jsx
@@ -2,32 +2,32 @@ import { useState } from 'react'
 
 const NoteForm = ({ createNote }) => {
 
-    const [newNote, setNewNote] = useState('')
+  const [newNote, setNewNote] = useState('')
 
-    const handleNoteChange = (e) => {
-        setNewNote(e.target.value);
-      }
+  const handleNoteChange = (e) => {
+    setNewNote(e.target.value)
+  }
 
-      const addNote = (e) => {
-        e.preventDefault();
-        const content = newNote
-        const noteObject = {
-          content,
-          important: true,
-        }
-    
-        setNewNote('')
-        createNote(noteObject)
-      }
-    
-return (
+  const addNote = (e) => {
+    e.preventDefault()
+    const content = newNote
+    const noteObject = {
+      content,
+      important: true,
+    }
+
+    setNewNote('')
+    createNote(noteObject)
+  }
+
+  return (
     <form onSubmit={addNote}>
       <input value={newNote}
         onChange={handleNoteChange}
         placeholder='write note content here' />
       <button type="submit">save</button>
     </form>
-)
+  )
 }
 
 export default NoteForm
